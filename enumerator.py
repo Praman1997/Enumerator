077594e5626746cb3e5b5ddb4ad651f73a7bc664
Praman Kasliwal <32700438+Praman1997@users.noreply.github.com>
2019-02-25 02:54:53 +0530

    Add files via upload

---
+++ enumerator.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from geoip import geolite2
+import os
+import sys
+import socket
+
+hackertarget_logo = """
+ _____                                     _
+|  ___|                                   | |
+| |__ _ __  _   _ _ __ ___   ___ _ __ __ _| |_ ___  _ __
+|  __| '_ \| | | | '_ ` _ \ / _ \ '__/ _` | __/ _ \| '__|
+| |__| | | | |_| | | | | | |  __/ | | (_| | || (_) | |
+\____/_| |_|\__,_|_| |_| |_|\___|_|  \__,_|\__\___/|_|
+                                                         
+                    Praman Kasliwal
+ | github.com/praman1997 | linkedin.com/in/praman-kasliwal-12892b146 |
+"""
+
+menu = """
+[0] Exit
+[1] Traceroute
+[2] Ping Test
+[3] DNS Lookup
+[4] Find DNS Host
+[5] Find Shared DNS
+[6] Zone Transfer
+[7] Whois Lookup
+[8] IP Location Lookup
+[9] Reverse IP Lookup
+[10] TCP Port Scan
+[11] HTTP Header Check
+"""
+
+print hackertarget_logo
+print menu
+
+def run():
+
+    try:
+        choice = input("Which option number : ")
+
+        if choice == 1:
+            print("\n")
+            print("[+] Traceroute script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "traceroute "+target
+            os.system(command)
+
+        elif choice == 2:
+            print("\n")
+            print("[+] Ping Test script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "ping -c 4 "+str(target)
+            os.system(command)
+
+        elif choice == 3:
+            print("\n")
+            print("[+] DNS Lookup script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "dnsenum -v "+str(target)
+            os.system(command)
+
+        elif choice == 4:
+            print("\n")
+            print("[+] Find Host DNS script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "host -v "+str(target)
+            os.system(command)
+
+        elif choice == 5:
+            print("\n")
+            print("[+] Find Shared DNS script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "dnsrecon -wd "+str(target)
+            os.system(command)
+
+        elif choice == 6:
+            print("\n")
+            print("[+] Zone Transfer script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "dnsrecon -d "+str(target)+ " -t axfr"
+            os.system(command)
+
+        elif choice == 7:
+            print("\n")
+            print("[+] Whois Lookup script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "whois "+str(target)
+            os.system(command)
+
+        elif choice == 8:
+            print("\n")
+            print("[+] IP Location Lookup script running..")
+            target = raw_input("[+] Target : ")
+            match = geolite2.lookup(target)
+            result = "[IPInfo] IP Address: "+str(match.ip)+"\n[IPInfo] Country: "+str(match.country)+"\n[IPInfo] Continent:"+str(match.continent)+"\n[IPInfo] Subdivision:"+str(match.subdivisions)+"\n[IPInfo] Timezone:"+str(match.timezone)+"\n[IPInfo] Location:"+str(match.location)
+            print(result)
+
+        elif choice == 9:
+            print("\n")
+            print("[+] Reverse IP Lookup script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            reversed_ip = socket.gethostbyaddr(target)
+            print(str(reversed_ip[0]))
+
+        elif choice == 10:
+            print("\n")
+            print("[+] TCP Port Scan script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            command = "sudo nmap -A -p- "+str(target)
+            os.system(command)
+
+        elif choice == 11:
+            print("\n")
+            print("[+] HTTP Header Check script running..")
+            target = raw_input("[+] Target : ")
+            print("\n")
+            request = requests.get(target)
+            txt = request.headers
+            print(str(txt))
+
+        elif choice == 0:
+            exit()
+
+    except KeyboardInterrupt:
+        print("\n[-] Aborted!")
+        quit()
+
+run()
